e551d4e85aa5ab0ca40c5df90d64fce482e7d488
Cysaac <justin-j@mail.de>
2026-05-15 09:05:30 +0200

    Struktur

---
+++ app/Views/Layouts/app.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang='de'>
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <title>Projekt</title>
+  <link rel='stylesheet' href='main.css'>
+</head>
+<body>
+
+<main>
+  
+</main>
+
+</body>
+</html>
